687b61e2c7af1c62b6c328d921f80ddfab00bbeb
Artur <admin@deswolf.com>
2019-12-18 19:01:09 +0000

    initial styling and resets

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,34 @@ import { User } from "./types/user.type";
 
 export type Props = {
   users: User[];
+  size: number;
+  [otherProps: string]: any;
 };
 
-const UserList: React.ReactNode = ({ users }: Props) => {
+export type UserItemProps = {
+  user: User;
+  size: number;
+};
+
+const UserItem: React.ElementType = ({ user, size }: UserItemProps) => {
   return (
-    <div className="react-user-list__wrapper">
-      <ul>
-        {users.map((user: User, i: number) => (
-          <li key={i}>{user.firstName}</li>
-        ))}
-      </ul>
-    </div>
+    <li>
+      <div
+        className="react-user-list__avatar"
+        style={{ width: `${size}px`, height: `${size}px`, lineHeight: `${size}px` }}>
+        {user.firstName.charAt(0) + user.lastName.charAt(0)}
+      </div>
+    </li>
+  );
+};
+
+const UserList: React.ReactNode = ({ users, size = 80, ...props }: Props) => {
+  return (
+    <ul className="react-user-list__wrapper" {...props}>
+      {users.map((user: User, i: number) => (
+        <UserItem key={i} user={user} size={size} />
+      ))}
+    </ul>
   );
 };
 
