ff8574ea68eddb15122091ba6de4335bf8af811a
Artur <admin@deswolf.com>
2020-03-02 17:11:52 +0000

    Custom user template for userlist

--- example/src/App.js
+++ example/src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
 import { USERS } from "./SAMPLE_DATA/users";
-import UserList, { UserItem } from 'react-user-list'
+import UserList, { UserAvatar } from 'react-user-list'
 
 export default class App extends Component {
   render () {
     return (
       <div style={{width: "80%", margin: "72px auto"}}>
-        <UserList users={USERS}>
-          {USERS.map((user) => <div><UserItem user={user}/><span style={{whiteSpace: "nowrap"}}>{user.firstName + " " + user.lastName}</span></div>)}
-        </UserList>
-        <div><UserItem user={USERS[1]} size={40} borderWidth={5} borderColor="rgba(0,0,0,0.5)" borderRadius={6} /></div>
+        <section className="section">
+          <UserList users={USERS}>
+          {({ index, user }) => {
+            return <div onClick={() => alert("test")}><UserAvatar user={user}/><span>{user.firstName + " " + user.lastName}</span></div>
+          }}
+          </UserList>
+        </section>
+        <section className="section">
+          <div><UserAvatar user={USERS[1]} size={80} borderWidth={5} borderColor="rgba(0,0,0,0.5)" borderRadius={6} /></div>
+        </section>
       </div>
     )
   }
