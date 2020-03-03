26dfb149099f93f135efcfecbc54ed2053f68f72
Artur <admin@deswolf.com>
2020-03-03 14:45:10 +0000

    add docusaurus

---
+++ example/docusaurus.config.js
@@ -0,0 +1,95 @@
+const package = require("./package.json");
+
+const title = "React User List";
+
+module.exports = {
+  title: title,
+  tagline: "The tagline of my site",
+  url: package.homepage,
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: package.author, // Usually your GitHub org/user name.
+  projectName: package.name, // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: title,
+      logo: {
+        alt: "Logo",
+        src: "img/logo.svg"
+      },
+      links: [
+        { to: "docs/doc1", label: "Docs", position: "left" },
+        { to: "examples", label: "Examples", position: "left"},
+        // { to: "blog", label: "Blog", position: "left" },
+        {
+          href: package.repository.url,
+          label: "GitHub",
+          position: "right"
+        }
+      ]
+    },
+    footer: {
+      // style: "dark",
+      // links: [
+      //   {
+      //     title: "Docs",
+      //     items: [
+      //       {
+      //         label: "Style Guide",
+      //         to: "docs/doc1"
+      //       },
+      //       {
+      //         label: "Second Doc",
+      //         to: "docs/doc2"
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     title: "Community",
+      //     items: [
+      //       {
+      //         label: "Stack Overflow",
+      //         href: "https://stackoverflow.com/questions/tagged/docusaurus"
+      //       },
+      //       {
+      //         label: "Discord",
+      //         href: "https://discordapp.com/invite/docusaurus"
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     title: "Social",
+      //     items: [
+      //       {
+      //         label: "Blog",
+      //         to: "blog"
+      //       },
+      //       {
+      //         label: "GitHub",
+      //         href: "https://github.com/facebook/docusaurus"
+      //       },
+      //       {
+      //         label: "Twitter",
+      //         href: "https://twitter.com/docusaurus"
+      //       }
+      //     ]
+      //   }
+      // ],
+      copyright: `Copyright © ${new Date().getFullYear()} ${title}. Docs built with Docusaurus.`
+    }
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: `${package.repository.url.split(".git")[0]}/edit/master/example/`
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      }
+    ]
+  ]
+};
---
+++ example/sidebars.js
@@ -0,0 +1,7 @@
+module.exports = {
+  someSidebar: {
+    'Get Started': ['installation'],
+    'API Reference': ['doc1', 'doc2', 'doc3'],
+    Features: ['mdx'],
+  },
+};
